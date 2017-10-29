61de38e2b981dbc75cc4e748f3cd9cd23aea9da0
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 12:36:10 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,20 +1,13 @@
-function onSave() {
-  $(function () {
-    $("save").on("click", function() {
-      var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-      $.get("/products/" + nextId + ".json", function(data) {
-        $(".productName").text(data["name"]);
-        $(".productPrice").text(data["price"]);
-        $(".productDescription").text(data["description"]);
-        $(".productInventory").text(data["inventory"]);
-        // re-set the id to current on the link
-        $(".js-next").attr("data-id", data["id"]);
-      });
-    });
-  });
+$('#save').click(() => saveGame())
+$('#previous').click(() => previousGame())
+$('#clear').click(() => clearGame())
+
+
+function saveGame() {
+  
 }
 
-function onPrevious() {
+function previousGame() {
   $(function () {
     $("#previous").on("click", function() {
       var prevId = parseInt($(".js-prev").attr("data-id")) - 1;
